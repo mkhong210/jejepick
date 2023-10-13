31081bf4ddbf5ae352ae8d4cc3c75a00da666b37
Minidrago <socoool2@gmail.com>
2023-10-13 09:49:51 +0900

    cho

--- app/pages/personal-test/page.js
+++ app/pages/personal-test/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import style from './personalTest.module.scss'
+import Test from '@/app/components/Test'
 
 function page() {
 	return (
 		<>
-			<div>page</div>
+			<Test />
 		</>
 	)
 }
