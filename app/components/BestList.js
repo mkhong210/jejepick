0a1a228fb36da1693124b6f81d3315140b17af79
jeonga214 <jeonga020214@gmail.com>
2023-10-18 16:46:33 +0900

    10/18

--- app/components/BestList.js
+++ app/components/BestList.js
@@ -17,12 +17,30 @@ export default function BestList () {
   const [aaa, setAaa] = useState({ data1: null, data2: null });
 
   const {testResultValue} = useContext(MyContext);
-  console.log(testResultValue );
+
+  //api 데이터
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  
+  async function getData() {
+      const result = await axios.get('/api/visit');
+      const newData = result.data;
+     
+      setData(newData);
+      setLoading(false);
+  }
+  
+  useEffect(() => {
+    getData();
+  }, [])
+
+  console.log(data);
+  
   // 첫 번째 요청 (로그인내용)
   useEffect(() => {
     if (loginID) {
       axios.get(`/server_api/ja?id=${loginID}`)
-        .then((response) => {
+      .then((response) => {
           setAaa((prevData) => ({
             ...prevData,
             data1: response.data,
@@ -31,14 +49,14 @@ export default function BestList () {
         .catch((error) => {
           console.log('Error:', error);
         });
-    }
-  }, [loginID]);
+      }
+    }, [loginID]);
 
   // 두 번째 요청(성향테스트결과)
   useEffect(() => {
     if (loginID) {
       axios.get(`/server_api/jaresult?profile=${loginID}`)
-        .then((response) => {
+      .then((response) => {
           setAaa((prevData) => ({
             ...prevData,
             data2: response.data,
@@ -47,43 +65,46 @@ export default function BestList () {
         .catch((error) => {
           console.log('Error:', error);
         });
+      }
+    }, [loginID]);
+    
+    let myName = null;
+    let tendency = null;
+    let image = null;
+    let tags = null;
+    let bestlist = null;
+    
+    if (aaa.data1 && aaa.data1.length > 0) {
+      myName = aaa.data1[0].name;
+      
     }
-  }, [loginID]);
-
-  let myName = null;
-  let tendency = null;
-  let image = null;
-  let tags = null;
-  let bestlist = null;
-
-  if (aaa.data1 && aaa.data1.length > 0) {
-    myName = aaa.data1[0].name;
-    console.log(myName);
-  }
-
-  if (aaa.data2 && aaa.data2.length > 0) {
-    const parsedProfileData = JSON.parse(aaa.data2[0].contents);
-    bestlist= aaa.data2[0].keywords;
-    tendency = parsedProfileData.tendency;
-    image = parsedProfileData.image;
-
-    console.log(bestlist);
-
-    if (Array.isArray(parsedProfileData.tag)) {
+    
+    if (aaa.data2 && aaa.data2.length > 0) {
+      const parsedProfileData = JSON.parse(aaa.data2[0].contents);
+      bestlist = JSON.parse(aaa.data2[0].keywords);
+      tendency = parsedProfileData.tendency;
+      image = parsedProfileData.image;
+      
+      console.log(bestlist);
+      
+      if (Array.isArray(parsedProfileData.tag)) {
         tags = parsedProfileData.tag.map(tag => tag.trim());
       } else if (typeof parsedProfileData.tag === 'string') {
         tags = parsedProfileData.tag.split(',').map(tag => tag.trim());
       } else {
         console.error('Unexpected format for tags:', parsedProfileData.tag);
       }
-
-    console.log(tags[0]);
-  }
-
+      
+      
+    }
   
-
-  return (
-    <>
+    
+    if (loading) {
+        return <div>로딩 중...</div>;
+    }
+    
+    return (
+      <>
 	  <div className={style.best}>
       <div className={style.contents_bestlist_back}>
         <div className={style.contents_profile + ` inner`}>
@@ -125,21 +146,21 @@ export default function BestList () {
           <div className={style.listcon}>
             <h2><img src="/asset/image/bestlist/marker1.svg"/>숙소 추천</h2>
           </div>
-          <List bestlist={bestlist}/>
+          <List bestlist={[bestlist[1],bestlist[2]]} data={data[0]} />
         </div>
 
         <div className={style.list}>
           <div className={style.listcon}>
             <h2><img src="/asset/image/bestlist/marker2.svg"/>맛집 추천</h2>
           </div>
-          <List bestlist={bestlist}/>
+          <List bestlist={[bestlist[3],bestlist[4]]} data={data[1]} />
         </div>
 
         <div className={style.list}>
           <div className={style.listcon}>
             <h2><img src="/asset/image/bestlist/marker3.svg"/>명소 추천</h2>
           </div>
-          <List bestlist={bestlist}/>
+          <List bestlist={[bestlist[0],bestlist[4]]} data={data[2]}/>
         </div>
 
         </div>
--- app/components/list/List.js
+++ app/components/list/List.js
@@ -2,66 +2,76 @@ import React, { useState, useEffect } from 'react';
 import style from '../../pages/list/list.module.scss'
 import ListItem from './ListItem'
 import Link from 'next/link'
+import Heart from '../Heart';
 
-function List({bestlist}) {
-
-	if (!bestlist) {
+function List({bestlist,data}) {
+	
+let [ddd,setDdd] = useState([]);
+	
+	if (!bestlist || !data) {
 		return <div>Loading...</div>;
 	}
 	
 	console.log(bestlist);
+	console.log(data);
 
-	// axios.get('/api/visit')
-	// .then(response => {
-	// 	const filteredJejuData = response.data;
-	// 	// 이 데이터를 상태로 설정하거나 다른 곳에서 사용
-	// })
-	// .catch(error => {
-	// 	console.error('Failed to fetch data:', error);
-	// });
+	//빈배열바꾸기
+	for (let i = 0; i < bestlist.length; i++) {
+		if(bestlist[i] === "X" ){
+			bestlist[i] = "";
+		}
+		
+	}
+
+
+	function filter(e) {
+		
+		let filteredData = data ? data.filter(obj => obj.alltag && obj.alltag.includes(bestlist[0]) && obj.alltag.includes(bestlist[1])) : [];
+		// console.log(filteredData);
+		setDdd(filteredData)
+	}
+	useEffect(()=>{
+		filter();
+	},[])
+
+	// 무작위로 3개의 요소 선택
+	function getRandomItems(arr, numItems) {
+		const shuffled = arr.sort(() => 0.5 - Math.random());
+		return shuffled.slice(0, numItems);
+	}
+	
+	const randomItems = getRandomItems(ddd, 3);
+	
+
+
+
+	
+	// function filter(e) {
+		
+	// 	let filteredData = data ? data.filter(obj => obj.alltag && obj.alltag.includes('휴식') && obj.alltag.includes('편의점') && obj.contentscd.label.includes('숙박')) : [];
+	// 	// console.log(filteredData);
+	// 	setData(filteredData)
+	// }
+	// useEffect(()=>{
+	// 	filter();
+	// },[])
 
 	return (
 		<>
 			<ul className={style.list}>
-				<li className={style.list_item}>
-					<Link href='#' className='item_wrap'>
-						<div className='img_wrap'>
-							<img src='/asset/common/item_example_thum.jpg'></img>
-						</div>
-						<div className='text_wrap'>
-							<p>똘똘이 국밥집</p>
-							<button className='like'>
-								<img src='/asset/common/Icon_favorite.svg' />
-							</button>
-						</div>
-					</Link>
-				</li>
-				<li className={style.list_item}>
-					<Link href='#' className='item_wrap'>
-						<div className='img_wrap'>
-							<img src='/asset/common/item_example_thum.jpg'></img>
-						</div>
-						<div className='text_wrap'>
-							<p>똘똘이 국밥집</p>
-							<button className='like'>
-								<img src='/asset/common/Icon_favorite.svg' />
-							</button>
-						</div>
-					</Link>
-				</li>
-				<li className={style.list_item}>
-					<Link href='#' className='item_wrap'>
-						<div className='img_wrap'>
-							<img src='/asset/common/item_example_thum.jpg'></img>
-						</div>
-						<div className='text_wrap'>
-							<p>똘똘이 국밥집</p>
-							<button className='like'>
-								<img src='/asset/common/Icon_favorite.svg' />
-							</button>
-						</div>
-					</Link>
-				</li>
+				{randomItems.map((item, k) => (
+					<li className={style.list_item} key={k}>
+						<Link href='#' className='item_wrap'>
+							<div className='img_wrap'>
+								<img src={item.repPhoto.photoid.imgpath}></img>
+							</div>
+							<div className='text_wrap'>
+								<p>{item.title}</p>
+								<Heart/>
+							</div>
+						</Link>
+					</li>
+				))}
 			</ul>
 		</>
 	)
