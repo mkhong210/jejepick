f7f61b7c8e747324176e740df466f76879fad010
mongmonghehe <dlwldud40344@gmail.com>
2023-10-18 17:57:46 +0900

    23.10.18  반응형 작업

--- app/pages/personal-start/page.js
+++ app/pages/personal-start/page.js
@@ -1,38 +1,53 @@
-"use client"
-import React, { useEffect } from 'react'
-import style from './personalStart.module.scss'
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useEffect } from "react";
+import style from "./personalStart.module.scss";
+import { useRouter } from "next/navigation";
 
 function page() {
   useEffect(() => {
-		const head = document.getElementsByClassName(`header`);
-		head[0].classList.add(style.hidden);
-		const btm = document.getElementsByClassName(`bottom`);
-		btm[0].classList.add(style.hidden);
-	}, []);
+    const head = document.getElementsByClassName(`header`);
+    head[0].classList.add(style.hidden);
+    const btm = document.getElementsByClassName(`bottom`);
+    btm[0].classList.add(style.hidden);
+  }, []);
 
   const router = useRouter();
 
-  function next(){
-    router.push('/pages/personal-test')
+  function next() {
+    router.push("/pages/personal-test");
   }
 
   return (
     <>
       <div className={style.testbefore}>
-        <div className={style.testbeforeback}></div>
-        <div className={style.whiteback}></div>
-        <div className={style.startcon}>
-          <div className={style.startjeje}></div>
-          <div className={style.con}>
-            <div><span>나</span>에게 맞는 <span>여행지</span>는 어디일까?</div>
-            <p>제제픽에서 성향 테스트 진단 결과로<br/>맞춤 여행지를 추천해드려요!</p>
+        <div className={style.content + ` inner`}>
+          <div className={style.contentbox}>
+            <div className={style.contentbox_logo}>
+              <div className={style.contentbox_logo_img}>
+                <img src="/asset/common/logo_2.svg" />
+              </div>
+              <div className={style.contentbox_txt}>
+                <p>
+                  <span>나</span>에게 맞는 <span>여행지</span>는 어디일까?
+                </p>
+                <p>
+                  제제픽에서 성향 테스트 진단 결과로
+                  <br />
+                  맞춤 여행지를 추천해드려요!
+                </p>
+              </div>
+            </div>
+            <div className={style.contentbox_img}>
+              <img src="/asset/image/IMG_jejudo_bg.png" />
+            </div>
+          </div>
+          <div className={style.startbtn} onClick={next}>
+            <p>테스트 시작하기</p>
           </div>
         </div>
-        <div className={style.start} onClick={next}>테스트 시작하기</div>
       </div>
     </>
-  )
+  );
 }
 
-export default page
+export default page;
