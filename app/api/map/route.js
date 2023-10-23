3c136d15fb578ad584dfe8634046114ab4fb370c
mkhong210 <ghdalsrud210@gmail.com>
2023-10-23 12:14:02 +0900

    23.10.23 맵 최종 제발

--- app/api/map/route.js
+++ app/api/map/route.js
@@ -5,7 +5,7 @@ export async function GET(req){
     console.log(param)
     
 
-    const REST_API_KEY = '29a94b5dabf06b6cdf709e81067b1f75';
+    const REST_API_KEY = 'c528248b7e7766216d83483fea8e2360';
     const url = `https://apis-navi.kakaomobility.com/v1/directions?${param}`;
     const map = await axios(url,{
         headers:{
@@ -20,7 +20,7 @@ export async function GET(req){
 export async function POST(req){   
     const d = await req.json();
     
-    const REST_API_KEY = '29a94b5dabf06b6cdf709e81067b1f75';
+    const REST_API_KEY = 'c528248b7e7766216d83483fea8e2360';
     const url = `https://apis-navi.kakaomobility.com/v1/waypoints/directions`;
     const map = await axios({
         method:'post',
