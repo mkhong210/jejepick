acb3791648cdd325dcdecea227fc85bcf7756c9d
mkhong210 <ghdalsrud210@gmail.com>
2023-10-25 16:44:17 +0900

    23.10.25 map api키 변경

--- app/api/map/route.js
+++ app/api/map/route.js
@@ -3,7 +3,8 @@ export async function GET(req) {
 	// 경로탐색 관련 맵
 	const param = req.url.split('?')[1];
 
-	const REST_API_KEY = '29a94b5dabf06b6cdf709e81067b1f75';
+	// RESTAPI 키 
+	const REST_API_KEY = '59d0e2ff6419b9a8f970c7409a781500';
 	const url = `https://apis-navi.kakaomobility.com/v1/directions?${param}`;
 	const map = await axios(url, {
 		headers: {
@@ -17,7 +18,8 @@ export async function GET(req) {
 export async function POST(req) {
 	const d = await req.json();
 
-	const REST_API_KEY = '29a94b5dabf06b6cdf709e81067b1f75';
+	// RESTAPI 키 
+	const REST_API_KEY = '59d0e2ff6419b9a8f970c7409a781500';
 	const url = `https://apis-navi.kakaomobility.com/v1/waypoints/directions`;
 	const map = await axios({
 		method: 'post',
